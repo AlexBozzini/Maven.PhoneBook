1f7f522418b1748bc79f3b749479d5d8589c2524
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-02-19 13:59:14 -0500

    Initial commit

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.phonebook;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private TreeMap<String, ArrayList> directory;
+    public PhoneBook(){
+        this.directory = new TreeMap<String, ArrayList>();
+    }
+    public void add (String name, ArrayList<String> phoneNumber){
+        directory.put(name, phoneNumber);
+        }
+    public void remove (String name, ArrayList<String> phoneNumber){
+
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,36 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void addTest (){
+        // given
+        String name = "Test1";
+        ArrayList<String> phoneNumber = "(302)372-8261", null;
+        PhoneBook phoneBook = new PhoneBook();
+        // when
+        phoneBook.add(name, phoneNumber);
+        // then
+        String actual = phoneBook.lookup(name);
+        Assert.assertEquals(phoneNumber, actual);
+    }
+
+    @Test
+    public void removeTest (){
+        // given
+        String name = "Test1";
+        String phoneNumber = "(302)372-8261";
+        PhoneBook phoneBook = new PhoneBook();
+        // when
+        phoneBook.remove(name, phoneNumber);
+        // then
+    }
 }
