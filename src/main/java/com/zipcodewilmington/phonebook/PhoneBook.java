90b5f5205bd4bbf8963ad006dac5c8fed1bab71b
Alexandra Bozzini <alexandrabozzini@zipcodersmbp74.lan>
2019-02-20 21:51:43 -0500

    made phonebook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,24 +1,64 @@
 package com.zipcodewilmington.phonebook;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
-/**
- * Created by leon on 1/23/18.
- */
 public class PhoneBook {
-    private TreeMap<String, ArrayList> directory;
+
+    private Map<String, List<String>> numberList;
+
+    public PhoneBook(Map<String, List<String>> numberList){
+       this.numberList = numberList;
+    }
+
     public PhoneBook(){
-        this.directory = new TreeMap<String, ArrayList>();
+        this.numberList = new TreeMap<String, List<String>>();
     }
-    public void add (String name, ArrayList<String> phoneNumber){
-        directory.put(name, phoneNumber);
+
+    public List<String> lookup(String name) {
+        return numberList.get(name);
+    }
+
+    public void add(String name, String phoneNumber) {
+        List<String> numbers = lookup(name);
+
+        if (numbers == null){
+            numbers = new ArrayList<String>();
         }
-    public void remove (String name){
-        directory.remove(name);
+
+        numbers.add(phoneNumber);
+        numberList.put(name, numbers);
     }
-    public void lookup (String name){
-        directory.get(name);
+
+    public void remove(String name) {
+        numberList.remove(name);
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        for (String key: numberList.keySet()) {
+            List<String> foundNumber = numberList.get(key);
+
+            if (foundNumber.contains(phoneNumber)){
+                return key;
+            }
+
+        }
+        return null;
+    }
+
+    public String listNamesAndNumbers() {
+        StringBuilder listOfNames = new StringBuilder();
+
+        for (String key : numberList.keySet()){
+            listOfNames.append(key);
+            listOfNames.append("\n");
+            for (String number : numberList.get(key)){
+                listOfNames.append(number);
+                listOfNames.append("\n");
+            }
+        }
+        return listOfNames.toString();
     }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -3,51 +3,135 @@ package com.zipcodewilmington.phonebook;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
-
     @Test
     public void addTest (){
         // given
-        String name = "Test1";
-        ArrayList<String> phoneNumber = new ArrayList<String>();
-        phoneNumber.add("(302)372-8261");
         PhoneBook phoneBook = new PhoneBook();
+        List<String> expected = Arrays.asList("(281)231-1839");
         // when
-        phoneBook.add(name, phoneNumber);
+        phoneBook.add("SamplePerson", "(281)231-1839");
+        List<String> actual = phoneBook.lookup("SamplePerson");
         // then
-        String actual = phoneBook.lookup(name);
-        Assert.assertEquals(phoneNumber, actual);
+        Assert.assertEquals(expected, actual);
+
     }
 
+    @Test
+    public void addTest2 (){
+        // given
+        PhoneBook phoneBook = new PhoneBook();
+        List<String> expected = Arrays.asList("(729)816-1364");
+        // when
+        phoneBook.add("SamplePerson", "(729)816-1364");
+        List<String> actual = phoneBook.lookup("SamplePerson");
+        // then
+        Assert.assertEquals(expected, actual);
+
+    }
     @Test
     public void removeTest (){
         // given
-        String name = "Test1";
-        PhoneBook phoneBook = new PhoneBook();
+        Map<String, List<String>> phoneNumberMap = new TreeMap<String, List<String>>();
+        phoneNumberMap.put("SamplePerson", Arrays.asList("(281)231-1839"));
+        PhoneBook phoneBook = new PhoneBook(phoneNumberMap);
         // when
-        phoneBook.remove(name);
+        phoneBook.remove("SamplePerson");
+        List<String> actual = phoneBook.lookup("SamplePerson");
         // then
-        String actual = phoneBook.lookup(name);
-        Assert.assertNotEquals(name, actual);
+        Assert.assertNull(actual);
     }
 
     @Test
-    public void lookupTest(){
+    public void lookupTest (){
         // given
-        String name = "Test1";
-        ArrayList<String> phoneNumber = new ArrayList<String>();
-        phoneNumber.add("(302)372-8261");
-        PhoneBook phoneBook = new PhoneBook();
+        Map<String, List<String>> phoneNumberMap = new TreeMap<String, List<String>>();
+        phoneNumberMap.put("SamplePerson", Arrays.asList("(281)231-1839"));
+        PhoneBook phoneBook = new PhoneBook(phoneNumberMap);
+        List<String> expected = Arrays.asList("(281)231-1839");
         // when
-        phoneBook.add(name, phoneNumber);
-        ArrayList<String> expected = phoneNumber;
+        List<String> actual = phoneBook.lookup("SamplePerson");
         // then
-        String actual = phoneBook.lookup(name);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lookupTest2 (){
+        // given
+        Map<String, List<String>> phoneNumberMap = new TreeMap<String, List<String>>();
+        phoneNumberMap.put("SamplePerson", Arrays.asList("(729)816-1364"));
+        PhoneBook phoneBook = new PhoneBook(phoneNumberMap);
+        List<String> expected = Arrays.asList("(729)816-1364");
+        // when
+        List<String> actual = phoneBook.lookup("SamplePerson");
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseLookupTest (){
+        // given
+        Map<String, List<String>> phoneNumberMap = new TreeMap<String, List<String>>();
+        phoneNumberMap.put("SamplePerson", Arrays.asList("(281)231-1839"));
+        PhoneBook phoneBook = new PhoneBook(phoneNumberMap);
+        String expected = "SamplePerson";
+        // when
+        String actual = phoneBook.reverseLookup("(281)231-1839");
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseLookupTest2 (){
+        // given
+        Map<String, List<String>> phoneNumberMap = new TreeMap<String, List<String>>();
+        phoneNumberMap.put("SamplePerson2", Arrays.asList("(281)231-1839"));
+        PhoneBook phoneBook = new PhoneBook(phoneNumberMap);
+        String expected = "SamplePerson2";
+        // when
+        String actual = phoneBook.reverseLookup("(281)231-1839");
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void listNamesAndNumbersTest(){
+        // given
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Zebra", "111-222-333");
+        phoneBook.add("Zebra", "139-131-1343");
+        phoneBook.add("Dog", "222-444-4444");
+        phoneBook.add("Dog", "239-139-1341");
+        String expected = "Dog\n222-444-4444\n239-139-1341\n" +
+                "Zebra\n111-222-333\n139-131-1343\n";
+        // when
+        String actual = phoneBook.listNamesAndNumbers();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void listNamesAndNumbersTest2(){
+        // given
+        Map<String, List<String>> phoneNumberMap = new TreeMap<String, List<String>>();
+        List<String> numberList1 = new ArrayList<String>();
+        numberList1.add("(382)083-1834");
+        phoneNumberMap.put("SamplePerson", numberList1);
+        List<String> numberList2 = new ArrayList<String>();
+        numberList2.add("(113)134-1341");
+        phoneNumberMap.put("SamplePerson2", numberList2);
+        PhoneBook phoneBook = new PhoneBook(phoneNumberMap);
+
+        String expected = "SamplePerson\n(382)083-1834\n" +
+                "SamplePerson2\n(113)134-1341\n";
+        // when
+        String actual = phoneBook.listNamesAndNumbers();
+        // then
         Assert.assertEquals(expected, actual);
     }
 }
